0e270b6fd2ea7729e8f3cef237506c144eb983d3
Felismino Chico <felisminochico35@gmail.com>
2023-12-26 12:23:08 +0000

    Atualizei o estilo da impressora

--- javascript/estagio.js
+++ javascript/estagio.js
@@ -2,6 +2,9 @@
 
 function carregar(){
     let hora = new Date().getHours();
+    let cabecalho = window.document.getElementsByTagName('header');
 
-    
+    if(hora > 10){
+        cabecalho.style.backgroundColor = '#012E40'
+    }
 }
