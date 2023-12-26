37eb0e79d6d8e8afed83393ca7131e718aacb340
Felismino Chico <felisminochico35@gmail.com>
2023-12-26 13:04:52 +0000

    Configurei o JavaScript

--- javascript/estagio.js
+++ javascript/estagio.js
@@ -2,9 +2,15 @@
 
 function carregar(){
     let hora = new Date().getHours();
-    let cabecalho = window.document.getElementsByTagName('header');
+    let cabecalho = window.document.getElementsByTagName('header')[0];
+    let titulo = window.document.getElementsByTagName('h1')[1];
+    let rodape = window.document.getElementsByTagName('footer')[0];
 
-    if(hora > 10){
+    hora = 1
+
+    if(hora > 18 || hora <= 6){
         cabecalho.style.backgroundColor = '#012E40'
+        titulo.style.color = '#012E40';
+        rodape.style.backgroundColor = '#012E40'
     }
 }
